9cbdf1ee68328674972e8817377804a4d6203ab5
Caleb Scutt <cnscutt1@sheffield.ac.uk>
2026-06-23 17:03:42 +0100

    fix time sereies func

--- analyses/first_script.R
+++ analyses/first_script.R
@@ -3,28 +3,33 @@ run_dates <- list.dirs("../data/g2g_data/Sim_g2g_run/", full.names = FALSE, recu
 tsfiles <- paste0(run_dates, "/base_.dat_WA")
 tsfiles_filtered <- tsfiles[19:20] ## starts at 2018
 tspath <- "../data/g2g_data/Sim_g2g_run/"
-tsdatX <- lapply(tsfiles_filtered,read.simple.file,path=tspath)
-ret=data.frame(DATE_TIME= (ISOdatetime((tsdatX[[1]])$Year,(tsdatX[[1]])$Month,(tsdatX[[1]])$Day, 0,0,0,tz="GMT") +60*(tsdatX[[1]])$"Time"))
-model_only=TRUE
+
+obs_mod_ts <- read_timeseries(tsfiles_filtered,tspath, model_only = FALSE )
+## remember to unit test
+
+# tsdatX <- lapply(tsfiles_filtered,read.simple.file,path=tspath)
+
+
+
+# ret=data.frame(DATE_TIME= (ISOdatetime((tsdatX[[1]])$Year,(tsdatX[[1]])$Month,(tsdatX[[1]])$Day, 0,0,0,tz="GMT") +60*(tsdatX[[1]])$"Time"))
+# model_only=TRUE
 
 
 
 
-
-
-
-for(i in 1:length(tsdatX)){
-    names(tsdatX[[i]])=gsub("^X", "",  names(tsdatX[[i]]))
-    DT = (ISOdatetime((tsdatX[[i]])$Year,(tsdatX[[i]])$Month,(tsdatX[[i]])$Day, 0,0,0,tz="GMT") +60*(tsdatX[[i]])$Time)
-    if(model_only){
-      tsdatX[[i]]=tsdatX[[i]][,c(1,grep("_Mod",names(tsdatX[[i]])))]
-      names(tsdatX[[i]])=gsub("_Mod$", "",  names(tsdatX[[i]]) )
-    }
-    tsdatX[[i]][,"DATE_TIME"]=DT
+# for(i in 1:length(tsdatX)){
+#     names(tsdatX[[i]])=gsub("^X", "",  names(tsdatX[[i]]))
+#     DT = (ISOdatetime((tsdatX[[i]])$Year,(tsdatX[[i]])$Month,(tsdatX[[i]])$Day, 0,0,0,tz="GMT") +60*(tsdatX[[i]])$Time)
+#     if(model_only){
+#       tsdatX[[i]]=tsdatX[[i]][,c(1,grep("_Mod",names(tsdatX[[i]])))]
+#       names(tsdatX[[i]])=gsub("_Mod$", "",  names(tsdatX[[i]]) )
+#     }
+#     tsdatX[[i]][,"DATE_TIME"]=DT
     
-    #ret=merge(ret,   subset(tsdatX[[i]] , select = -c(Step,Year, Month, Day, Time, En)),by="DATE_TIME")
-    ret=merge(ret,    tsdatX[[i]][, !names(tsdatX[[i]]) %in% c("Step","Year", "Month", "Day", "Time", "En") ]   ,by="DATE_TIME")
-}
+#     #ret=merge(ret,   subset(tsdatX[[i]] , select = -c(Step,Year, Month, Day, Time, En)),by="DATE_TIME")
+#     ret=merge(ret,    tsdatX[[i]][, !names(tsdatX[[i]]) %in% c("Step","Year", "Month", "Day", "Time", "En") ]   ,by="DATE_TIME")
+# }
+#     ret=merge(ret,    tsdatX[[i]][, !names(tsdatX[[i]]) %in% c("Step","Year", "Month", "Day", "Time", "En") ]   ,by="DATE_TIME")
 
 
 
@@ -35,6 +40,6 @@ for(i in 1:length(tsdatX)){
 
 
 
-## date range we need is 2018 to 2021
+# ## date range we need is 2018 to 2021
 
 
--- functions/utility.R
+++ functions/utility.R
@@ -1,24 +1,62 @@
-read_timeseries<-function(tsfiles,tspath,model_only=T){
-  tsdatX=lapply(tsfiles,read.simple.file,path=tspath)
-  ret=data.frame(DATE_TIME= (ISOdatetime((tsdatX[[1]])$"Year",(tsdatX[[1]])$"Month",(tsdatX[[1]])$"Day", 0,0,0,tz="GMT") +60*(tsdatX[[1]])$"Time"))
-  for(i in 1:length(tsdatX)){
-    names(tsdatX[[i]])=gsub("^X", "",  names(tsdatX[[i]]))
-    DT = (ISOdatetime((tsdatX[[i]])$"Year",(tsdatX[[i]])$"Month",(tsdatX[[i]])$"Day", 0,0,0,tz="GMT") +60*(tsdatX[[i]])$"Time")
-    if(model_only){
-      tsdatX[[i]]=tsdatX[[i]][,c(1,grep("_Mod",names(tsdatX[[i]])))]
-      names(tsdatX[[i]])=gsub("_Mod$", "",  names(tsdatX[[i]]) )
+read_timeseries <- function(tsfiles, tspath, model_only = TRUE) {
+  
+  # 1. Read all files into a temporary list
+  tsdatX <- lapply(tsfiles, read.simple.file, path = tspath)
+  processed_list <- list()
+  
+  for(i in 1:length(tsdatX)) {
+    df <- tsdatX[[i]]
+    
+    # Clean up column names (remove leading 'X' if R added it)
+    names(df) <- gsub("^X", "", names(df))
+    
+    # Calculate the proper DATE_TIME column
+    DT <- ISOdatetime(df$Year, df$Month, df$Day, 0, 0, 0, tz = "GMT") + (60 * df$Time)
+    df$DATE_TIME <- DT
+    
+    # Filter for model columns if requested
+    if (model_only) {
+      # Keep DATE_TIME and any column ending in _Mod
+      mod_cols <- grep("_Mod", names(df), value = TRUE)
+      df <- df[, c("DATE_TIME", mod_cols), drop = FALSE]
+      
+      # Strip the "_Mod" suffix from the names
+      names(df) <- gsub("_Mod$", "", names(df))
     }
-    tsdatX[[i]][,"DATE_TIME"]=DT
     
-    #ret=merge(ret,   subset(tsdatX[[i]] , select = -c(Step,Year, Month, Day, Time, En)),by="DATE_TIME")
-    ret=merge(ret,    tsdatX[[i]][, !names(tsdatX[[i]]) %in% c("Step","Year", "Month", "Day", "Time", "En") ]   ,by="DATE_TIME")
+    # Remove unwanted metadata columns safely (Note the correct comma placement here)
+    cols_to_remove <- c("Step", "Year", "Month", "Day", "Time", "En")
+    df <- df[, !names(df) %in% cols_to_remove, drop = FALSE]
     
+    # OPTIONAL FIX FOR NAME CLASHES: 
+    # If importing multiple files, append the filename as a suffix to the variables
+    if (length(tsfiles) > 1) {
+      file_id <- gsub("\\.dat.*$", "", tsfiles[i]) # Strips file extension
+      var_idx <- names(df) != "DATE_TIME"
+      names(df)[var_idx] <- paste0(names(df)[var_idx], "_", file_id)
+    }
+    
+    processed_list[[i]] <- df
   }
+  
+  # 2. Elegantly merge all processed data frames by DATE_TIME all at once
+  # (all = TRUE ensures no time-steps are accidentally dropped if data is missing)
+  ret <- Reduce(function(x, y) merge(x, y, by = "DATE_TIME", all = TRUE), processed_list)
+  
   return(ret)
 }
 
-read.simple.file<-function(path,filename,sep=",",exclude=NULL,...) {
+## unit test it
 
+
+read.time.series <- function(tsfiles, tspath){
+   tsdatX <- lapply(tsfiles,read.simple.file,path=tspath)
+    ret <- data.frame(DATE_TIME= (ISOdatetime((tsdatX[[1]])$"Year",(tsdatX[[1]])$"Month",(tsdatX[[1]])$"Day", 0,0,0,tz="GMT") +60*(tsdatX[[1]])$"Time"))
+
+
+}
+
+read.simple.file<-function(path,filename,sep=",",exclude=NULL,...) {
 fname<-paste(path,filename,sep="")
 tdata<-read.csv(fname,header=T,sep=sep,...)
 tdata[tdata< -999]<-NA
