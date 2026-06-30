86cd9aebdaacd350d583e0846ec3e5a809669800
Caleb Scutt <cnscutt1@sheffield.ac.uk>
2026-06-30 13:36:20 +0100

    using patchwork  instead for plotting layout

--- functions/make.hydrograph.r
+++ functions/make.hydrograph.r
@@ -10,13 +10,16 @@ make.hydrograph <-function(event.data, g2g.id, storm.name, cdata, qt.data){
   #   stop(match_call$event.data, " must be a data.frame...\n")
   # }
 
-  river_name <- wales_cdata[G2G.ID == g2g.id, ]$River.Name
+  river_name <- cdata[G2G.ID == g2g.id, ]$River.Name
+  site_name <- cdata[G2G.ID == g2g.id, ]$Site.Name.
+  area <- cdata[G2G.ID == g2g.id, ]$Area.
   qt <- qt.data[[storm.name]][G2G.ID == g2g.id, ]
   qmed <- qt$qmed
-  max_discharge <- max(c(event.data$mod[,get(paste0(g2g.id, "_Mod")) ], event.data$obs[,get(paste0(g2g.id, "_Obs")) ]))
-  qt_only_vals <-  qt[, c(5:14), with = FALSE]
-  highest_qt_exceeded_index <- max(which(max_discharge > qt_only_vals))
-  qt_val <- qt_only_vals[, highest_qt_exceeded_index, with = FALSE]
+  max_discharge <- max(c(event.data$mod[,get(paste0(g2g.id, "_Mod"))], event.data$obs[, get(paste0(g2g.id, "_Obs"))]))
+  qt_only_vals <-  qt[, .(q5, q10, q25, q50, q75, q100, q200, q250, q1000)]
+  highest_qt_exceeded_index <- which(max_discharge > qt_only_vals)
+  qt_vals <- qt_only_vals[, highest_qt_exceeded_index, with = FALSE]
+  qt_df <- data.frame(qt = names(qt_vals), value = unlist(qt_vals))
 
   # if (!is.null(river_name))
 
@@ -29,45 +32,47 @@ make.hydrograph <-function(event.data, g2g.id, storm.name, cdata, qt.data){
   geom_line(data = event.data$obs, 
             aes(x = DATE_TIME, y = get(paste0(g2g.id, "_Obs")), color = "Observed"), linewidth = 0.8) +
 
-  geom_hline(yintercept = qmed, color = "darkgreen", linetype = "dotted") +
+  geom_hline(yintercept = qmed, color = "darkgreen", linetype = "dotted", linewidth = 0.4) +
 
   annotate(
     "text", 
     x = min(event.data$obs$DATE_TIME, na.rm = TRUE),
     y = qmed, 
-    label = "qmed", 
-    vjust = -0.8, 
-    hjust = 0, 
-    color = "darkgreen", 
+    label = "qmed",
+    vjust = -0.8,
+    hjust = 0,
+    color = "darkgreen",
     fontface = "bold"
   ) +
-
-
-  geom_hline(yintercept = qt_val[[1]], color = "blue", linetype = "dashed") +
   
-  # LAYER 2: The Text Label
-  annotate(
-    "text", 
-    x = min(event.data$obs$DATE_TIME, na.rm = TRUE), 
-    y = qt_val[[1]], 
-    label = names(qt_val)[1], 
-    vjust = -0.8, 
-    hjust = 0, 
-    color = "blue", 
-    fontface = "bold"
-  ) +
+  geom_hline(data = qt_df, aes(yintercept =qt_df$value), inherit.aes = FALSE,color = "blue", linetype = "dashed", linewidth = 0.4) +
+  
+  geom_text(
+      data = qt_df,
+      aes(
+        x = min(event.data$obs$DATE_TIME, na.rm = TRUE),
+        y = qt_df$value,
+        label = qt_df$qt
+      ),
+      inherit.aes = FALSE,
+      hjust = 0,
+      vjust = -0.8,
+      colour = "blue",
+      fontface = "bold"
+    ) +
   # Labels and Scale adjustments
     labs(
-      title = paste(storm.name, "Hydrograph at g2g site:", g2g.id),
-      subtitle = paste("River:", river_name),
-      x = "Date",
-      y = "Streamflow") +
+      title = paste(storm.name, "at", g2g.id),
+      subtitle = paste("River:", river_name, "Site:", site_name, "Area:", area),
+      x = "",
+      y = "Flow (m^3 s^-1)") +
   scale_color_manual(values = c("Model" = "red", "Observed" = "black")) +
   # Themes and Legend placement
   theme_bw() +
   scale_x_datetime(date_labels = "%d-%m-%Y") +
   theme(legend.position = c(0.8, 0.8),
-        legend.title    = element_blank())
+        legend.title    = element_blank(),
+        aspect.ratio = 0.5)
 
   return(p)
 }
